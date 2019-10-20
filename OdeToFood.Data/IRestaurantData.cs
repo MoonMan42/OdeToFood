4861ed571082958905fa312dde50367087a0c2be
MoonMan42 <miller42matt@gmail.com>
2019-10-19 21:03:27 -0400

    Setup in-memory development.

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,33 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAllRestaurants(); 
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{Id=1, Name="Scott\'s Pizza", Location="Atlanta", Cuisine=CuisineType.Itilian},
+                new Restaurant{Id=2, Name="Aladin\'s", Location="Cicago", Cuisine=CuisineType.Indian},
+                new Restaurant{Id=3, Name="Altons", Location="Dallas", Cuisine=CuisineType.Mexican}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAllRestaurants()
+        {
+            return from r in restaurants orderby r.Name select r;
+        }
+    }
+}
--- OdeToFood/Pages/Restaurant/List.cshtml.cs
+++ OdeToFood/Pages/Restaurant/List.cshtml.cs
@@ -4,14 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Data;
+using OdeToFood.Core;
 
 namespace OdeToFood.Pages.Restaurant
 {
     public class ListModel : PageModel
     {
+        private readonly IRestaurantData restaurant;
+        public IEnumerable<OdeToFood.Core.Restaurant> Restaurants { get; set; }
+
+        public ListModel(IRestaurantData restaurant)
+        {
+            this.restaurant = restaurant;
+        }
+
         public void OnGet()
         {
-
+            Restaurants = restaurant.GetAllRestaurants();
         }
+
+
     }
 }
