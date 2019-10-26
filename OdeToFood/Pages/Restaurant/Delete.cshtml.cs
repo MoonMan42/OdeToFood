fc45b70ecb1a458a995784bd4224fd7154d4534e
MoonMan42 <miller42matt@gmail.com>
2019-10-26 16:39:04 -0400

    Added deleted page.

---
+++ OdeToFood/Pages/Restaurant/Delete.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurant
+{
+    public class DeleteModel : PageModel
+    {
+        private IRestaurantData restaurantData;
+
+
+        public Core.Restaurant Restaurant { get; set; }
+
+        public DeleteModel(IRestaurantData data)
+        {
+            this.restaurantData = data;
+        }
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = restaurantData.GetRestaurantById(restaurantId);
+
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            return Page();
+        }
+
+        public IActionResult OnPost(int restaurantId)
+        {
+            var restaurant = restaurantData.Delete(restaurantId);
+            restaurantData.Commit();
+
+            if (restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            TempData["Message"] = $"{restaurant.Name} deleted";
+            return RedirectToPage("./List");
+        }
+    }
+}
